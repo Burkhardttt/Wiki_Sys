d62a0ad25dd5ab7e8a36a12131c5b9a0897da50c
lxc <xichenli96@gmail.com>
2023-02-05 14:29:37 -0500

    2.5 Modification of banner.txt

--- src/main/java/com/burkhardt/wiki/WikiApplication.java
+++ src/main/java/com/burkhardt/wiki/WikiApplication.java
@@ -2,6 +2,7 @@ package com.burkhardt.wiki;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
@@ -13,6 +14,7 @@ public class WikiApplication {
 	private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(WikiApplication.class);
+//		app.setBannerMode(Banner.Mode.OFF);
 		Environment env = app.run(args).getEnvironment();
 		LOG.info("Start successfully ~ :)");
 		LOG.info("Address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));

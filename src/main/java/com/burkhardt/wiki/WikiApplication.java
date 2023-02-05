522be92074d45051362fbab07f1b2c821525aea2
lxc <xichenli96@gmail.com>
2023-02-05 14:16:50 -0500

    2.5 Modification of info of Starting app

--- src/main/java/com/burkhardt/wiki/WikiApplication.java
+++ src/main/java/com/burkhardt/wiki/WikiApplication.java
@@ -1,13 +1,21 @@
 package com.burkhardt.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import javax.swing.*;
 
 @SpringBootApplication
 public class WikiApplication {
-
+	private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(WikiApplication.class, args);
+		SpringApplication app = new SpringApplication(WikiApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("Start successfully ~ :)");
+		LOG.info("Address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 
 }

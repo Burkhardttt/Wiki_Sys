b45408e805bef6057f5acd5ce06169f3d415f0c8
lxc <xichenli96@gmail.com>
2023-02-06 16:49:29 -0500

    3-6 add a common response class: CommonResp all return types from backend would be CommonResp

--- src/main/java/com/burkhardt/wiki/controller/EbookController.java
+++ src/main/java/com/burkhardt/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.burkhardt.wiki.controller;
 
 import com.burkhardt.wiki.domain.Ebook;
+import com.burkhardt.wiki.resp.CommonResp;
 import com.burkhardt.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,11 @@ public class EbookController {
 	private EbookService ebookService;
 
 	@GetMapping("/list")
-	public List<Ebook> list(){
-		return ebookService.list();
+	public CommonResp list(){
+		CommonResp<List<Ebook> > resp = new CommonResp<>();
+		List<Ebook> list = ebookService.list();
+		resp.setContent(list);
+		return resp;
 	}
 
 }

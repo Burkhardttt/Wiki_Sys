9fbbc7dd5c8b734c997fe79f3f4497ca7db8ec43
lxc <xichenli96@gmail.com>
2023-02-06 17:35:04 -0500

    3-8 make a tools CopyUtil.java to encapsulate BeanUtils

--- src/main/java/com/burkhardt/wiki/service/EbookService.java
+++ src/main/java/com/burkhardt/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.burkhardt.wiki.domain.EbookExample;
 import com.burkhardt.wiki.mapper.EbookMapper;
 import com.burkhardt.wiki.req.EbookReq;
 import com.burkhardt.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.burkhardt.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,12 +23,16 @@ public class EbookService {
 		criteria.andNameLike("%" + req.getName() + "%");
 		List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-		List<EbookResp> respList = new ArrayList<>();
-		for (Ebook ebook : ebookList) {
-			EbookResp ebookResp = new EbookResp();
-			BeanUtils.copyProperties(ebook, ebookResp);
-			respList.add(ebookResp);
-		}
-		return respList;
+//		List<EbookResp> respList = new ArrayList<>();
+//		for (Ebook ebook : ebookList) {
+////			EbookResp ebookResp = new EbookResp();
+////			BeanUtils.copyProperties(ebook, ebookResp);
+//			EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//			respList.add(ebookResp); // single object copy
+//		}
+		// list copy
+		List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+
+		return list;
 	}
 }

2d19f846ba6c6e1f8162adf03c1446a565d5eb69
lxc <xichenli96@gmail.com>
2023-02-06 17:24:03 -0500

    3-7 encapsulate request params and return params

--- src/main/java/com/burkhardt/wiki/service/EbookService.java
+++ src/main/java/com/burkhardt/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.burkhardt.wiki.service;
 
 import com.burkhardt.wiki.domain.Ebook;
+import com.burkhardt.wiki.domain.EbookExample;
 import com.burkhardt.wiki.mapper.EbookMapper;
+import com.burkhardt.wiki.req.EbookReq;
+import com.burkhardt.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,7 +18,18 @@ public class EbookService {
 	@Resource
 	private EbookMapper ebookMapper;
 
-	public List<Ebook> list() {
-		return ebookMapper.selectByExample(null);
+	public List<EbookResp> list(EbookReq req) {
+		EbookExample ebookExample = new EbookExample();
+		EbookExample.Criteria criteria = ebookExample.createCriteria();
+		criteria.andNameLike("%" + req.getName() + "%");
+		List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+		List<EbookResp> respList = new ArrayList<>();
+		for (Ebook ebook : ebookList) {
+			EbookResp ebookResp = new EbookResp();
+			BeanUtils.copyProperties(ebook, ebookResp);
+			respList.add(ebookResp);
+		}
+		return respList;
 	}
 }

e88aa2d679c1ce6e07b053f73b4a679ba7b4489a
lxc <xichenli96@gmail.com>
2023-02-10 21:00:17 -0500

    5-6 config interceptor of axios, print request param and return params

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,21 +2,38 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import AntD from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 import * as Icons from '@ant-design/icons-vue';
-import axios from "axios";
+import axios from 'axios';
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+    console.log('请求参数：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('返回结果：', response);
+    return response;
+}, error => {
+    console.log('返回错误：', error);
+    return Promise.reject(error);
+});
+
 const app = createApp(App);
-app.use(store).use(router).use(AntD).mount('#app');
+app.use(store).use(router).use(Antd).mount('#app');
 
 // 全局使用图标
-const icons:any = Icons;
+const icons: any = Icons;
 for (const i in icons) {
     app.component(i, icons[i]);
 }
 
-console.log('env:', process.env.NODE_ENV);
-console.log('server:', process.env.VUE_APP_SERVER);
+console.log('环境：', process.env.NODE_ENV);
+console.log('服务端：', process.env.VUE_APP_SERVER);

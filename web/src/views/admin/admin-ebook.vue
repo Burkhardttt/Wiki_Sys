8f1f83b6e5c6020b2dc9a9a383646d26a697dd90
lxc <xichenli96@gmail.com>
2023-02-10 23:11:07 -0500

    6-3 some modifications on ebook management page

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -2,4 +2,13 @@
   <div class="about">
     <h1>Ebook Management</h1>
   </div>
+  <a-layout>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <div class="about">
+        <h1>Ebook Management</h1>
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>

b01f6c9ff2b4f1d9c6b18f6c6f0aae9bced61841
lxc <xichenli96@gmail.com>
2023-02-10 20:27:06 -0500

    5-5 modified axios's baseURL

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -84,8 +84,8 @@ export default defineComponent({
     const ebooks1 = reactive({books: []});
 
     onMounted(() => {
-      console.log("onMounted");
-      axios.get(process.env.VUE_APP_SERVER + "/ebook/list").then((response) => {
+      console.log("onMounted22");
+      axios.get("/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content;
         ebooks1.books = data.content;

5b1a4685a254283da2f104f0a44aad3f3dfc6718
lxc <xichenli96@gmail.com>
2023-02-11 19:27:23 -0500

    6-4 PageHelper extension integrated mapper log level to trace, print all sql log

--- src/main/java/com/burkhardt/wiki/service/EbookService.java
+++ src/main/java/com/burkhardt/wiki/service/EbookService.java
@@ -6,6 +6,10 @@ import com.burkhardt.wiki.mapper.EbookMapper;
 import com.burkhardt.wiki.req.EbookReq;
 import com.burkhardt.wiki.resp.EbookResp;
 import com.burkhardt.wiki.util.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -15,16 +19,25 @@ import java.util.List;
 @Service
 public class EbookService {
 
+	private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
 	@Resource
 	private EbookMapper ebookMapper;
 
 	public List<EbookResp> list(EbookReq req) {
+
 		EbookExample ebookExample = new EbookExample();
 		EbookExample.Criteria criteria = ebookExample.createCriteria();
 		if (!ObjectUtils.isEmpty(req.getName())) {
 			criteria.andNameLike("%" + req.getName() + "%");
 		}
+
+		PageHelper.startPage(1, 3);
 		List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+		PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+		LOG.info("Total number of rows: " + pageInfo.getTotal());
+		LOG.info("Total number of pages: " + pageInfo.getPages());
 
 //		List<EbookResp> respList = new ArrayList<>();
 //		for (Ebook ebook : ebookList) {

f0986a271385cacbaf538a63d668a7c79f1d2fde
lxc <xichenli96@gmail.com>
2023-02-21 17:20:11 -0500

    9-4 Add check that the user name cannot be repeated and add custom exceptions

--- src/main/java/com/burkhardt/wiki/controller/ControllerExceptionHandler.java
+++ src/main/java/com/burkhardt/wiki/controller/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.burkhardt.wiki.controller;
 
+import com.burkhardt.wiki.exception.BusinessException;
 import com.burkhardt.wiki.resp.CommonResp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,9 +26,39 @@ public class ControllerExceptionHandler {
 	@ResponseBody
 	public CommonResp validExceptionHandler(BindException e) {
 		CommonResp commonResp = new CommonResp();
-		LOG.warn("param verification failed(参数校验失败)：{}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+		LOG.warn("参数校验失败：{}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
 		commonResp.setSuccess(false);
 		commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+		return commonResp;
+	}
+
+	/**
+	 * 校验异常统一处理
+	 * @param e
+	 * @return
+	 */
+	@ExceptionHandler(value = BusinessException.class)
+	@ResponseBody
+	public CommonResp validExceptionHandler(BusinessException e) {
+		CommonResp commonResp = new CommonResp();
+		LOG.warn("业务异常：{}", e.getCode().getDesc());
+		commonResp.setSuccess(false);
+		commonResp.setMessage(e.getCode().getDesc());
+		return commonResp;
+	}
+
+	/**
+	 * 校验异常统一处理
+	 * @param e
+	 * @return
+	 */
+	@ExceptionHandler(value = Exception.class)
+	@ResponseBody
+	public CommonResp validExceptionHandler(Exception e) {
+		CommonResp commonResp = new CommonResp();
+		LOG.error("系统异常：", e);
+		commonResp.setSuccess(false);
+		commonResp.setMessage("系统出现异常，请联系管理员");
 		return commonResp;
 	}
 }
--- src/main/java/com/burkhardt/wiki/service/UserService.java
+++ src/main/java/com/burkhardt/wiki/service/UserService.java
@@ -2,6 +2,8 @@ package com.burkhardt.wiki.service;
 
 import com.burkhardt.wiki.domain.User;
 import com.burkhardt.wiki.domain.UserExample;
+import com.burkhardt.wiki.exception.BusinessException;
+import com.burkhardt.wiki.exception.BusinessExceptionCode;
 import com.burkhardt.wiki.mapper.UserMapper;
 import com.burkhardt.wiki.req.UserQueryReq;
 import com.burkhardt.wiki.req.UserSaveReq;
@@ -14,6 +16,7 @@ import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
@@ -69,9 +72,15 @@ public class UserService {
 	public void save(UserSaveReq req) {
 		User user = CopyUtil.copy(req, User.class);
 		if (ObjectUtils.isEmpty(req.getId())) {
-			// 新增
-			user.setId(snowFlake.nextId());
-			userMapper.insert(user);
+			User userDB = selectByLoginName(req.getLoginName());
+			if (ObjectUtils.isEmpty(userDB)) {
+				// 新增
+				user.setId(snowFlake.nextId());
+				userMapper.insert(user);
+			} else {
+				// 用户名已存在
+				throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+			}
 		} else {
 			// 更新
 			userMapper.updateByPrimaryKey(user);
@@ -80,5 +89,17 @@ public class UserService {
 
 	public void delete(Long id) {
 		userMapper.deleteByPrimaryKey(id);
+	}
+
+	public User selectByLoginName(String LoginName) {
+		UserExample userExample = new UserExample();
+		UserExample.Criteria criteria = userExample.createCriteria();
+		criteria.andLoginNameEqualTo(LoginName);
+		List<User> userList = userMapper.selectByExample(userExample);
+		if (CollectionUtils.isEmpty(userList)) {
+			return null;
+		} else {
+			return userList.get(0);
+		}
 	}
 }

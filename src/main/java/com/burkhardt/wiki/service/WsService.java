fd46643bc3cab86ce6cb0d2aa74518d3eadc3e64
lxc <xichenli96@gmail.com>
2023-02-22 18:40:22 -0500

    10-10 Pass the business thread log pipeline number to the asynchronous thread to facilitate production operations and maintenance to check the problem

--- src/main/java/com/burkhardt/wiki/service/WsService.java
+++ src/main/java/com/burkhardt/wiki/service/WsService.java
@@ -1,6 +1,7 @@
 package com.burkhardt.wiki.service;
 
 import com.burkhardt.wiki.websocket.WebSocketServer;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,8 @@ public class WsService {
 	public WebSocketServer webSocketServer;
 
 	@Async
-	public void sendInfo(String message) {
+	public void sendInfo(String message, String logId) {
+		MDC.put("LOG_ID", logId);
 		webSocketServer.sendInfo(message);
 	}
 }

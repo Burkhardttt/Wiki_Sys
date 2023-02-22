60fb933c11d64b4b6b41aea93a14fdfcccc8d885
lxc <xichenli96@gmail.com>
2023-02-22 17:31:10 -0500

    10-6 Add timer to regularly execute eBook information update SQL, add log

--- src/main/java/com/burkhardt/wiki/mapper/DocMapperCust.java
+++ src/main/java/com/burkhardt/wiki/mapper/DocMapperCust.java
@@ -6,5 +6,7 @@ public interface DocMapperCust {
 
 	public void increaseViewCount(@Param("id") Long id);
 	public void increaseVoteCount(@Param("id") Long id);
+
+	public void updateEbookInfo();
 }
 
